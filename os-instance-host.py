8704828f64773ac30759ee875865937d4e16a7a8
Steve Brasier <steveb@stackhpc.com>
2024-05-02 13:11:06 +0000

    much more flexible output

--- os-instance-host.py
+++ os-instance-host.py
@@ -1,46 +1,57 @@
 #!/usr/bin/env python3
 
-import argparse, pprint
+import argparse, pprint, json, operator, dataclasses
 from tabulate import tabulate
 import openstack
 
-cli = argparse.ArgumentParser(description='Show instance hosts')
-cli.add_argument('network')
+cli = argparse.ArgumentParser(description='List ports on a given network with instance and hypervisor information.')
+cli.add_argument('network', help='name of network')
 cli.add_argument('-n', '--name', help='filter output to instances with names containing NAME')
+cli.add_argument('-f', '--format', choices=['table', 'json'], default='table', help='output format')
+cli.add_argument('-s', '--sort-by', choices=['compute_host'], help='sort output by')
 
+class EnhancedJSONEncoder(json.JSONEncoder):
+    #https://stackoverflow.com/a/51286749/916373
+        def default(self, o):
+            if dataclasses.is_dataclass(o):
+                return dataclasses.asdict(o)
+            return super().default(o)
+
+def to_json(o):
+    return json.dumps(o, cls=EnhancedJSONEncoder, indent=4)
+
+def to_table(o):
+    return tabulate(o, headers='keys', tablefmt='simple')
+
+@dataclasses.dataclass
 class PortInfo:
-    FIELDS = ('device_id', 'port_name', 'instance_name', 'compute_host')
-    def __init__(self):
-        for f in self.FIELDS:
-            setattr(self, f, None)
-
-    def __str__(self):
-        return ' '.join(getattr(self, f) or '-' for f in self.FIELDS)
-
+    device_id: str = ''
+    port_name: str = ''
+    instance_name: str = ''
+    compute_host: str = ''
+    
 if __name__ == '__main__':
     args = cli.parse_args()
     conn = openstack.connection.from_config()
     network = conn.network.find_network(args.network)
 
-hosts = {}
 ports = []
-for port in conn.network.ports(network_id=network.id):
-    portinfo = PortInfo()
-    if port.device_id != '':
-        portinfo.device_id = port.device_id
-    portinfo.port_name = port.name
-    if portinfo.device_id is not None:
+for port in conn.network.ports(): #network_id=network.id):
+    portinfo = PortInfo(device_id=port.device_id, port_name=port.name) # either might still be ''
+    if portinfo.device_id:
         instance = conn.compute.find_server(port.device_id)
         if instance is not None:
             portinfo.instance_name = instance.name
             portinfo.compute_host = instance.compute_host
-            if portinfo.compute_host not in hosts:
-                hosts[portinfo.compute_host] = []
-            hosts[portinfo.compute_host].append(portinfo.instance_name)
             ports.append(portinfo)
 
-if args.name is None: # output by-hosts:
-    pprint.pprint(hosts)
-else: # output by name:
-    instance_info = [(p.instance_name, p.compute_host) for p in ports if args.name in p.instance_name]
-    print(tabulate(instance_info, headers=('INSTANCE', 'HOST')))
+# output
+if args.name:
+    ports = [p for p in ports if args.name in p.instance_name]
+
+if args.sort_by == 'compute_host':
+    ports = sorted(ports, key=operator.attrgetter('compute_host'))
+if args.format == 'json':
+    print(to_json(ports))
+else:
+    print(to_table(ports))
